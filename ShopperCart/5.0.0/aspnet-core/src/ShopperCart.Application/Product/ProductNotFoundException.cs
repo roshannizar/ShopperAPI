67d6668b8e2ac9a40c8c83c6f1b5df035c8a1419
Roshan Nizar <41587533+roshannizar@users.noreply.github.com>
2019-12-10 13:05:39 +0530

    Create ProductNotFoundException.cs

---
+++ ShopperCart/5.0.0/aspnet-core/src/ShopperCart.Application/Product/ProductNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopperCart.Customer
+{
+    public class ProductNotFoundException:Exception
+    {
+        public ProductNotFoundException():base("Product Not Found!")
+        {
+
+        }
+    }
+}
