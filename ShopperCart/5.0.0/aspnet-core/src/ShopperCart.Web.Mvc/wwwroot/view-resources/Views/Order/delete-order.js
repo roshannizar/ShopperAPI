4636b8ca5645d8b543ee6d61ac0cda7b606b887a
Roshan Nizar <41587533+roshannizar@users.noreply.github.com>
2019-12-12 12:17:55 +0530

    Create delete-order.js

---
+++ ShopperCart/5.0.0/aspnet-core/src/ShopperCart.Web.Mvc/wwwroot/view-resources/Views/Order/delete-order.js
@@ -0,0 +1,34 @@
+﻿const spinner = document.getElementById("spinner");
+
+function showSpinner() {
+    spinner.style.visibility = 'visible';
+    spinner.style.display = 'inline';
+    spinner.style.opacity = '0.5';
+}
+
+function closeSpinner() {
+    spinner.style.visibility = 'hidden';
+    spinner.style.display = 'none';
+}
+
+function DeleteOrder(id) {
+
+    showSpinner();
+
+    $.ajax({
+        url: 'http://localhost:21021/api/v1/Order/'+parseInt(id),
+        dataType: 'json',
+        type: 'delete',
+        contentType: 'application/json',
+        data: parseInt(id),
+        processData: false,
+        success: function (data, textStatus, jQxhr) {
+            console.log(data, textStatus, jQxhr);
+            closeSpinner();
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            console.log(jqXhr, textStatus, errorThrown);
+            closeSpinner();
+        }
+    });
+}
