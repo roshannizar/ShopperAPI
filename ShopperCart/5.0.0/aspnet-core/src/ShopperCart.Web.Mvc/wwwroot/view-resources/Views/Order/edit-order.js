02c2adcc726206e9c327c82bcba878ee1c08d7cb
Roshan Nizar <41587533+roshannizar@users.noreply.github.com>
2019-12-12 12:17:49 +0530

    Update edit-order.js

--- ShopperCart/5.0.0/aspnet-core/src/ShopperCart.Web.Mvc/wwwroot/view-resources/Views/Order/edit-order.js
+++ ShopperCart/5.0.0/aspnet-core/src/ShopperCart.Web.Mvc/wwwroot/view-resources/Views/Order/edit-order.js
@@ -1,6 +1,18 @@
 ﻿var TempProduct = [];
 var breakCount = 0;
 var rowCount = 1;
+const spinner = document.getElementById("spinner");
+
+function showSpinner() {
+    spinner.style.visibility = 'visible';
+    spinner.style.display = 'inline';
+    spinner.style.opacity = '0.5';
+}
+
+function closeSpinner() {
+    spinner.style.visibility = 'hidden';
+    spinner.style.display = 'none';
+}
 
 function EditOrders(id) {
 
@@ -125,6 +137,8 @@ function Confirm() {
     order.CustomerId = parseInt(customerId);
     order.Date = currentDate.replace(/^"(.*)"$/, '$1');
 
+    showSpinner();
+
     $.ajax({
         url: 'http://localhost:21021/api/v1/Order',
         dataType: 'json',
@@ -134,6 +148,7 @@ function Confirm() {
         processData: false,
         success: function (data, textStatus, jQxhr) {
             console.log(data, textStatus, jQxhr);
+            closeSpinner();
         },
         error: function (jqXhr, textStatus, errorThrown) {
             console.log(jqXhr, textStatus, errorThrown);
