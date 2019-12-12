e53ce64aec087e1c60592a8978eaa934d16c4133
Roshan Nizar <41587533+roshannizar@users.noreply.github.com>
2019-12-12 15:17:07 +0530

    Update OrderController.cs

--- ShopperCart/5.0.0/aspnet-core/src/ShopperCart.Web.Mvc/Controllers/OrderController.cs
+++ ShopperCart/5.0.0/aspnet-core/src/ShopperCart.Web.Mvc/Controllers/OrderController.cs
@@ -52,26 +52,6 @@ namespace ShopperCart.Web.Mvc.Controllers
             return View();
         }
 
-        [HttpPost]
-        public IActionResult CreateOrder([FromBody]OrderViewModel orderViewModel)
-        {
-            try
-            {
-                orderViewModel.Status = StatusTypeViewModel.Pending;
-                var order = ObjectMapper.Map<OrderDto>(orderViewModel);
-                //Create Order
-                orderService.CreateOrder(order);
-
-                TempData["Message"] = "Order has been added successfully!";
-                return RedirectToAction("Index", "Order");
-            }
-            catch (Exception ex)
-            {
-                TempData["Message"] = "Error Occured while creating an Order! " + ex;
-                throw ex;
-            }
-        }
-
         [HttpGet]
         public IActionResult ShowOrder(int id)
         {
@@ -95,48 +75,6 @@ namespace ShopperCart.Web.Mvc.Controllers
             {
                 TempData["Message"] = "No Order found! " + ex;
                 throw ex;
-            }
-        }
-
-        [HttpPost]
-        public IActionResult OrderDelete(int id)
-        {
-            try
-            {
-                orderService.DeleteOrder(id);
-                TempData["Message"] = "You have deleted the order Ref No: " + id + " successfully!";
-                return RedirectToAction("Index", "Order");
-            }
-            catch (Exception ex)
-            {
-                TempData["Message"] = "Error occured while deleting the order " + id + "! " + ex;
-                throw new Exception();
-            }
-        }
-
-        [HttpPost]
-        public IActionResult OrderEdit([FromBody]OrderViewModel orderViewModel)
-        {
-            try
-            {
-                if (!ModelState.IsValid)
-                    return View("OrderEdit");
-
-                if (!(orderViewModel.OrderItems.Count > 0))
-                    return RedirectToAction("Index");
-
-                var order = ObjectMapper.Map<OrderDto>(orderViewModel);
-                orderService.UpdateOrder(order);
-
-                TempData["Message"] = "Save changes made for order Ref No: " +
-                    order.OrderItems[0].OrderId + " successfully!";
-                return RedirectToAction("Index", "Order");
-
-            }
-            catch (Exception ex)
-            {
-                TempData["Message"] = "Error occured while updating the order! " + ex;
-                throw new Exception();
             }
         }
     }
