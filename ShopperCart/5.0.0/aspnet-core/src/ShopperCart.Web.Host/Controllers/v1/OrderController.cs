e7240dfd888e9d46daa6cc470afe064a87404181
Roshan Nizar <41587533+roshannizar@users.noreply.github.com>
2019-12-12 12:19:03 +0530

    Update OrderController.cs

--- ShopperCart/5.0.0/aspnet-core/src/ShopperCart.Web.Host/Controllers/v1/OrderController.cs
+++ ShopperCart/5.0.0/aspnet-core/src/ShopperCart.Web.Host/Controllers/v1/OrderController.cs
@@ -45,12 +45,54 @@ namespace ShopperCart.Web.Host.Controllers.v1
                 orderService.CreateOrder(order);
 
                 TempData["Message"] = "Order has been added successfully!";
-                return RedirectToAction("Index", "Order");
+                return Redirect("https://localhost:62114/Order/Get");
             }
             catch (Exception ex)
             {
                 TempData["Message"] = "Error Occured while creating an Order! " + ex;
                 throw ex;
+            }
+        }
+
+        [HttpPut]
+        public IActionResult UpdateOrder([FromBody] OrderViewModel orderViewModel)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return View("OrderEdit");
+
+                if (!(orderViewModel.OrderItems.Count > 0))
+                    return RedirectToAction("Index");
+
+                var order = ObjectMapper.Map<OrderDto>(orderViewModel);
+                orderService.UpdateOrder(order);
+
+                TempData["Message"] = "Save changes made for order Ref No: " +
+                    order.OrderItems[0].OrderId + " successfully!";
+                return RedirectToAction("Index", "Order");
+
+            }
+            catch (Exception ex)
+            {
+                TempData["Message"] = "Error occured while updating the order! " + ex;
+                throw new Exception();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteOrder(int id)
+        {
+            try
+            {
+                orderService.DeleteOrder(id);
+                TempData["Message"] = "You have deleted the order Ref No: " + id + " successfully!";
+                return RedirectToAction("Index", "Order");
+            }
+            catch (Exception ex)
+            {
+                TempData["Message"] = "Error occured while deleting the order " + id + "! " + ex;
+                throw new Exception();
             }
         }
     }
